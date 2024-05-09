d087b12831a18149a0f1009e2d04e21dbcb73525
MHendriF <hendrifebriansyah28@gmail.com>
2024-05-09 09:23:35 +0700

    sending email

--- sticky-breeze/routes/web.php
+++ sticky-breeze/routes/web.php
@@ -10,10 +10,14 @@ Route::get('/dashboard', Controllers\DashboardController::class)->middleware(['a
 Route::get('stores', [Controllers\StoreController::class, 'index'])->name('stores.index');
 
 Route::middleware('auth')->group(function () {
-    Route::resource('stores', Controllers\StoreController::class)->except('index');
+    Route::middleware(['verified'])->group(function () {
+        Route::resource('stores', Controllers\StoreController::class)->except('index'); 
+    });
     Route::get('/profile', [Controllers\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');    
 });
+
+
 
 require __DIR__.'/auth.php';
